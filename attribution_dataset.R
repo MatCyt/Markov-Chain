1b36e3a2158879486b46dfc018bac5d2f1294fb5
Mateusz Cytrowski <mateusz.cytrowski@lingarogroup.com>
2018-11-02 14:44:17 +0100

    final code - needs cleaning

--- attribution_dataset.R
+++ attribution_dataset.R
@@ -1,9 +1,10 @@
 # Dummy Dataset Creation for Marketing Attribution on Cookie Level using Markov Chains
 
 # Libraries ----
-library(dplyr)
+
 library(data.table)
 library(lubridate)
+library(dbplyr)
 
 # Cookie
 # Time
@@ -53,15 +54,68 @@ str(df.cut3)
 
 
 # Events
-table(df.cut3$event3)
+table(df.cut3$new_event)
 
-df.cut3$event3 = sample(c("Impression", "Conversion"), 1000000, replace = TRUE, prob = c(0.98, 0.02))
+df.cut3$new_event = sample(c("impression", "conversion"), 1000000, replace = TRUE, prob = c(0.98, 0.02))
+
+str(df.cut3)
+
+# Conversions
+df.cut3$new_conversion = 0
+df.cut3$new_conversion[df.cut3$new_event == "conversion"] = 1
+  
+table(df.cut3$new_conversion)
+
+
+
+# Conversion Value
+
+prices <- seq(from = 4, to = 8.5, by = 0.5)
+prices
+
+df.cut3$conversion_value = 0
+df.cut3$conversion_value[df.cut3$new_conversion == 1] = df.prices
+
+df.prices <- sample(prices, 19653, replace = TRUE,
+                    prob = c(0.05, 0.05, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1, 0.05, 0.05))
+
+# Channels
+df.cut3 %>%
+  group_by(channel) %>%
+  summarise(sum(new_conversion))
+
+df.cut3$channel = 0
+
+df.cut3 = 
+  df.cut3 %>%
+  mutate(channel = replace(channel, creative_name == "Amnet_DTH_all_creatives", "Social Media"),
+         channel = replace(channel, creative_name == "Agora_doublebillboard", "Paid Search"),
+         channel = replace(channel, creative_name == "Polsat_all_banners_CPC", "Online Video"),
+         channel = replace(channel, creative_name == "WP_SG_doublebillboard", "Online Display"),
+         channel = replace(channel, creative_name == "Interia_doublebillboard", "Paid Search"),
+         channel = replace(channel, creative_name == "Polsat_Double_overlay", "Paid Search"),
+         channel = replace(channel, creative_name == "Polsat_welcome_screen", "Online Display"),
+         channel = replace(channel, creative_name == "WP_sport,turystyka,film_doublebillboard", "Paid Search"),
+         channel = replace(channel, creative_name == "Interia_rectangle", "Paid Search"))
+
+
+
+table(df.cut3$channel)
+
+table(df.cut3$creative_name)
 
 # Dataset
 
-dataset = data.table(
-  cookie = 
-)
+str(df.cut3)
+
+final_data = 
+  df.cut3 %>%
+  select(cookie3, time2, new_event, new_conversion, conversion_value, channel) %>%
+  mutate(device_type = device, country = country, city = city)
+
+colnames(final_data) = c("cookie", "time", "interaction", "conversion", "conversion_value", "channel", "device_type", "country", "city")
+
+str(final_data)
 
 # Explore the dataset
 
