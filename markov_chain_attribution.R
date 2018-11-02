648474e3b949990c8e5f6ea74068b097723b3937
MatCyt <mateusz.cytrowski@gmail.com>
2018-11-02 19:43:40 +0100

    adding working files v1 for markov attribution

---
+++ markov_chain_attribution.R
@@ -0,0 +1,32 @@
+### Load Libraries ----
+
+
+### Load Datasets ----
+
+
+
+### Prepare the files - Split Paths ----
+
+
+
+### Markov Chain and Heuristic Models ----
+
+
+
+### Prepare final joint dataset ----
+
+# Join results
+
+# Calculate ROAS
+
+
+### Visualize the results ----
+
+# Channel contribution
+
+# Optimal budget allocation - ROAS based
+
+# Markov Chain vs Heuristics models
+
+# Markov network graph
+
