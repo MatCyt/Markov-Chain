b1b6ded184251a3a1c7bd501171094e98c2c16b0
MatCyt <mateusz.cytrowski@gmail.com>
2018-11-14 17:16:22 +0100

    markov_attribution - initial commit

--- attibution_dummy_dataset.R
+++ attibution_dummy_dataset.R
@@ -8,11 +8,14 @@ library(dplyr)
 
 # Read the dataset and delete some columns----
 
-df.initial = fread("C:\\Users\\mateu\\Desktop\\MarketingAttribution\\sample_dataset_main.csv")
+df.initial = fread("C:\\Users\\matcyt\\Desktop\\MarketingAttribution\\sample_dataset_main.csv")
 
 # Work laptop
 df.initial = fread("C:\\Users\\mateusz.cytrowski\\Desktop\\MediaProject\\Datasets\\sample_dataset_main.csv")
 
+
+
+# Cut unnecessary columns
 df.cut = df.initial[, c("cookie", "time", "event", "creative_name", "deviceType_name", "country_name", "conversion")]
 
 ## DATA PREP: Cookie, Country, City, Device, Time ----
@@ -24,11 +27,8 @@ df.cut$cookie3 = substr(df.cut$cookie2, 4, 28)
 
 df.cut = df.cut[, -c("cookie", "cookie2")]
 
-# Country & City & Device
+# Country & Device
 country = "Germany"
-
-cities = c("Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt")
-city = sample(cities, 1000000, replace = TRUE, prob = c(0.42, 0.2, 0.16, 0.13, 0.1)) #based on population
 
 devices = c("PC", "Mobile", "Tablet", "SmartTV")
 device = sample(devices, 1000000, replace = TRUE, prob = c(0.35, 0.6, 0.04, 0.01))
@@ -47,14 +47,14 @@ df.cut2 =
 df.cut3 = df.cut2[-sample(1:nrow(df.cut2), 750792), ]
 
 
-
 # Events & Conversions ----
-df.cut3$new_event = sample(c("impression", "conversion"), 1000000, replace = TRUE, prob = c(0.98, 0.02))
+df.cut3$new_event = sample(c("impression", "conversion"), 1000000, replace = TRUE, prob = c(0.97, 0.03))
 
 # Conversions
 df.cut3$new_conversion = 0
 df.cut3$new_conversion[df.cut3$new_event == "conversion"] = 1
-  
+
+
 # Conversion Value ----
 
 prices <- seq(from = 4, to = 8.5, by = 0.5)
@@ -66,7 +66,7 @@ df.cut3$conversion_value[df.cut3$new_conversion == 1] = df.prices
 
 # Channels ----
 df.cut3 %>%
-  group_by(channel) %>%
+  group_by(creative_name) %>%
   summarise(sum(new_conversion))
 
 df.cut3$channel = 0
@@ -89,16 +89,15 @@ df.cut3 =
 final_data = 
   df.cut3 %>%
   select(cookie3, time2, new_event, new_conversion, conversion_value, channel) %>%
-  mutate(device_type = device, country = country, city = city)
+  mutate(device_type = device, country = country)
 
-colnames(final_data) = c("cookie", "time", "interaction", "conversion", "conversion_value", "channel", "device_type", "country", "city")
+colnames(final_data) = c("cookie", "time", "interaction", "conversion", "conversion_value", "channel", "device_type", "country")
 
-str(final_data)
 
 # Save the file
 
 # Home laptop
-write.csv2(final_data, file = "C:\\Users\\mateu\\Desktop\\MarketingAttribution\\attribution_markov_dataset.csv")
+write.csv2(final_data, file = "C:\\Users\\matcyt\\Desktop\\MarketingAttribution\\attribution_markov_dataset.csv")
 
 # Work laptop
 write.csv2(final_data, file = "C:\\Users\\mateusz.cytrowski\\Desktop\\Github\\attribution_markov_dataset.csv")
--- markov_chain_attribution.R
+++ markov_chain_attribution.R
@@ -4,36 +4,65 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(data.table, dplyr, ChannelAttribution, ggplot2)
 
-# test
-# testing test
-
 
 ### Load Datasets ----
-campaign_data = fread()
-campaign_budget = fread()
+campaign_data = fread("C:/Users/matcyt/Desktop/MarketingAttribution/attribution_markov_dataset.csv")
+campaign_budget_daily = fread("C:/Users/matcyt/Desktop/MarketingAttribution/attribution_budget_daily.csv", dec = ",")
 
 ### Prepare the files - Split Paths ----
+df_split = campaign_data %>%
+  group_by(cookie) %>%
+  arrange(time) %>%
+  mutate(path_no = ifelse(is.na(lag(cumsum(conversion))), 0, lag(cumsum(conversion))) + 1) %>%
+  ungroup() %>%
+  mutate(path_id = paste0(cookie, path_no))
 
 
+### Prepare the file - Create the paths ----
+df_paths = df_split %>%
+  group_by(path_id) %>%
+  arrange(time) %>%
+  summarise(path = paste(channel, collapse = ">"),
+            total_conversions = sum(conversion)) %>%
+  ungroup()
 
 ### Markov Chain and Heuristic Models ----
+markov_attribution <- markov_model(df_paths,
+                             var_path = "path",
+                             var_conv = "total_conversions",
+                             var_value = NULL,
+                             var_null = NULL,
+                             out_more = TRUE)
 
+
+heuristic_attribution <- heuristic_models(df_paths,
+                                     var_path = "path",
+                                     var_conv = "total_conversions")
 
 
 ### Prepare final joint dataset ----
 
-# Join results
+# Join attribution results
+all_model_results = merge(markov_attribution$result, heuristic_attribution)
 
-# Calculate ROAS
+# Aggregate budget
+campaign_budget_total = as.data.table(
+  campaign_budget_daily %>%
+    group_by(channel) %>%
+    summarise(total_cost = round(sum(cost), 1))
+)
+
+# Join into final results
+campaign_attribution = merge(all_model_results, campaign_budget_total, 
+                             by.x = "channel_name", by.y = "channel")
+
+#### Calculate ROAS
+campaign_attribution = 
+  campaign_attribution %>%
+  mutate(chanel_weight = (total_conversions / sum(total_conversions)),
+         cost_weight = (total_cost / sum(total_cost)),
+         roas = chanel_weight / cost_weight,
+         OptimalBudget = total_cost * roas)
 
 
-### Visualize the results ----
-
-# Channel contribution
-
-# Optimal budget allocation - ROAS based
-
-# Markov Chain vs Heuristics models
-
-# Markov network graph
-
+#### MultiChannel and one channel issue
